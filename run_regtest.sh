1708c298409e418d55a60c29d14e90556df4826d
sgiavasis <sgiava77@gmail.com>
2019-05-14 16:47:37 -0400

    Added a sanity check for the default pipeline at the end.

--- run_regtest.sh
+++ run_regtest.sh
@@ -11,3 +11,6 @@ sudo docker run -v /home/ubuntu:/home/ubuntu -v /home/ubuntu/CPAC_regtest_pack:/
 sudo docker run -v /home/ubuntu:/home/ubuntu -v /home/ubuntu/CPAC_regtest_pack:/home/ubuntu/CPAC_regtest_pack -v /media/ebs/fnirt:/output -v /media/ebs/fnirt/work:/tmp $1 /home/ubuntu /output participant  --data_config_file /home/ubuntu/CPAC_regtest_pack/data_config_regtest.yml --pipeline_file /home/ubuntu/CPAC_regtest_pack/regtest_pipeline_fnirt.yaml --n_cpus 2 --pipeline_override "numParticipantsAtOnce: 18" --save_working_dir
 
 sudo docker run -v /home/ubuntu:/home/ubuntu -v /home/ubuntu/CPAC_regtest_pack:/home/ubuntu/CPAC_regtest_pack -v /media/ebs/ants-spike:/output -v /media/ebs/ants-spike/work:/tmp $1 /home/ubuntu /output participant  --data_config_file /home/ubuntu/CPAC_regtest_pack/data_config_regtest.yml --pipeline_file /home/ubuntu/CPAC_regtest_pack/regtest_pipeline_ants-spike.yaml --n_cpus 2 --pipeline_override "numParticipantsAtOnce: 18" --save_working_dir
+
+# final sanity check on default pipeline, run just one participant from an S3 bucket
+sudo docker run -v /home/ubuntu:/home/ubuntu -v /home/ubuntu/CPAC_regtest_pack:/home/ubuntu/CPAC_regtest_pack -v /media/ebs/default:/output -v /media/ebs/default/work:/tmp $1 s3://fcp-indi/data/Projects/ADHD200/RawDataBIDS/KKI /output participant --n_cpus 20 --participant_ndx 1 --save_working_dir
