cbefb043aaea8e5a63363820803d138bb82d8005
Ubuntu <ubuntu@ip-172-31-5-228.ec2.internal>
2019-09-05 04:37:45 +0000

    Updated regtest pack with new directory structure and updated C-PAC pipeline configs.

--- run_quick_regtest.sh
+++ run_quick_regtest.sh
@@ -7,4 +7,4 @@
 #   36 cpus
 #   60 GB mem
 
-sudo docker run -v /home/ubuntu:/home/ubuntu -v /home/ubuntu/CPAC_regtest_pack:/home/ubuntu/CPAC_regtest_pack -v /media/ebs/$2/ants-quick:/output -v /media/ebs/$2/ants-quick/work:/tmp $1 /home/ubuntu /output participant  --data_config_file /home/ubuntu/CPAC_regtest_pack/data_config_regtest_quick.yml --pipeline_file /home/ubuntu/CPAC_regtest_pack/regtest_pipeline_ants.yaml --n_cpus 7 --pipeline_override "numParticipantsAtOnce: 5" --save_working_dir
+sudo docker run -v /home/ubuntu:/home/ubuntu -v /home/ubuntu/CPAC_regtest_pack:/home/ubuntu/CPAC_regtest_pack -v /media/ebs/$2/ants-quick:/output -v /media/ebs/$2/ants-quick/work:/tmp $1 /home/ubuntu /output participant  --data_config_file /home/ubuntu/CPAC_regtest_pack/data_config_regtest_quick.yml --pipeline_file /home/ubuntu/CPAC_regtest_pack/configs/$2/regtest_pipeline_ants.yml --n_cpus 7 --pipeline_override "numParticipantsAtOnce: 5" --save_working_dir
