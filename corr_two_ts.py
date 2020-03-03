84dec71da737e5391ba58f644287896622aaa11f
sgiavasis <sgiava77@gmail.com>
2020-03-03 21:37:04 +0000

    Better error check in corr_two_ts.py.

--- corr_two_ts.py
+++ corr_two_ts.py
@@ -11,6 +11,10 @@ fmriprep_func_img = nb.load(str(sys.argv[2]))
 cpac_func_data = cpac_func_img.get_data()
 fmriprep_func_data = fmriprep_func_img.get_data()
 
+if len(cpac_func_img.shape) < 4 or len(fmriprep_func_img.shape) < 4:
+    raise Exception("\n\n[!] At least one of the input files is not a " \
+                    "4D/time series dataset.\n")
+
 ts_corrs = []
 
 for i in range(0, cpac_func_img.shape[0]):
