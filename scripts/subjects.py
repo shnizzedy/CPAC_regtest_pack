d194862b94087cce33450f79fa784272f0c053a8
Jon Clucas <jon.clucas@childmind.org>
2020-03-04 18:02:23 -0500

    :sparkles: Handle lists of strings and no session specifications

--- scripts/subjects.py
+++ scripts/subjects.py
@@ -1,12 +1,12 @@
-def generate_subject_list_for_range(subject_start_stop, session_start_stop):
+def generate_subject_list_for_range(subject_start_stop, session_start_stop=None):
     """
     Function to create a subject list for a given range. All values are inclusive.
 
     Parameters
     ----------
-    subject_start_stop: 2-tuple of integers (start, stop)
+    subject_start_stop: 2-tuple of integers (start, stop) or list of specific values
 
-    session_start_stop: 2-tuple of integers (start, stop)
+    session_start_stop: 2-tuple of integers (start, stop) or list of specific values or None
 
     Returns
     -------
@@ -18,11 +18,34 @@ def generate_subject_list_for_range(subject_start_stop, session_start_stop):
     ['sub-0025427_ses-1', 'sub-0025428_ses-1', 'sub-0025427_ses-2', 'sub-0025428_ses-2']
     """
     return([
-        f'sub-00{sub}_ses-{ses}' for ses in range(
-            session_start_stop[0],
-            session_start_stop[1] + 1
-        ) for sub in range(
-            subject_start_stop[0],
-            subject_start_stop[1] + 1
-        )
+        f'sub-00{sub}{ses_string}' for ses_string in ([
+            f'_ses-{ses}' for ses in _expand_range(
+                session_start_stop
+            )
+        ] if session_start_stop else [
+            ''
+        ]) for sub in _expand_range(subject_start_stop)
     ])
+
+def _expand_range(tuple_or_list):
+    """
+    Function to expand an inclusive tuple to a range or return a literal list
+
+    Parameter
+    ---------
+    tuple_or_list: 2-tuple of integers or list
+
+    Returns
+    -------
+    list
+    """
+    return(
+        range(
+            tuple_or_list[0],
+            tuple_or_list[1] + 1
+        ) if all([
+            isinstance(tuple_or_list, tuple),
+            len(tuple_or_list)==2,
+            *[isinstance(v, int) for v in tuple_or_list]
+        ]) else list(tuple_or_list)
+    )
