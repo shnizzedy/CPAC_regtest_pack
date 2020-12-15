9056ef63cbe693f436c4ea8a5fee669f8d2e35f7
Jon Clucas <jon@vasegurt.com>
2020-12-15 11:30:58 -0500

    :recycle: Update dct_diff to handle nested config & importability

--- cpac_pipe_diff.py
+++ cpac_pipe_diff.py
@@ -1,12 +1,27 @@
-
 import yaml
 import math
 
 
 def read_yaml_file(yaml_file):
-    with open(yaml_file, 'r') as f:
-        yaml_dct = yaml.load(f)
-    return yaml_dct
+    '''Function to read a dict YAML file at a given path.
+
+    Parameters
+    ----------
+    yaml_file: str
+        path to YAML file
+
+    Returns
+    -------
+    dict
+        parsed YAML
+
+    Example
+    -------
+    >>> read_yaml_file('/code/dev/docker_data/default_pipeline.yml')[
+    ...     'pipeline_setup']['pipeline_name']
+    'cpac-default-pipeline'
+    '''
+    return yaml.safe_load(open(yaml_file, 'r'))
 
 
 def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
@@ -14,27 +29,54 @@ def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
 
 
 def dct_diff(dct1, dct2):
+    '''Function to compare 2 nested dicts, dropping values unspecified
+    in the second.
+
+    Parameters
+    ----------
+    dct1: dict
+
+    dct2: dict
+
+    Returns
+    -------
+    diff: set
+        a tuple of values from dct1, dct2 for each differing key
+
+    Example
+    -------
+    >>> pipeline = read_yaml_file('/code/dev/docker_data/default_pipeline.yml')
+    >>> dct_diff(pipeline, pipeline)
+    {}
+    >>> pipeline2 = read_yaml_file('/code/CPAC/resources/configs/'
+    ...     'pipeline_config_fmriprep-options.yml')
+    >>> dct_diff(pipeline, pipeline2)['pipeline_setup']['pipeline_name']
+    ('cpac-default-pipeline', 'cpac_fmriprep-options')
+    '''
     diff = {}
     for key in dct1:
+        if isinstance(dct1[key], dict):
+            diff[key] = dct_diff(dct1[key], dct2.get(key, {}))
+        else:
+            dct1_val = dct1.get(key)
+            dct2_val = dct2.get(key) if isinstance(dct2, dict) else None
 
-        if 'Directory' in key or 'maxCores' in key or 'numParticipants' in key \
-                or 'maximumMemory' in key or 'pipelineName' in key:
-            continue
-
-        dct1_val = dct1.get(key)
-        dct2_val = dct2.get(key)
-        
-        if isinstance(dct1_val, float) and isinstance(dct2_val, float):
-            if isclose(dct1_val, dct2_val):
+            # skip unspecified values
+            if dct2_val is None:
                 continue
-            else:
+
+            if isinstance(dct1_val, float) and isinstance(dct2_val, float):
+                if isclose(dct1_val, dct2_val):
+                    continue
+                else:
+                    diff[key] = (dct1_val, dct2_val)
+
+            if dct1_val != dct2_val:
                 diff[key] = (dct1_val, dct2_val)
 
-        if dct1_val != dct2_val:
-            diff[key] = (dct1_val, dct2_val)
+    # only return non-empty diffs
+    return {k: diff[k] for k in diff if diff[k]}
 
-    return diff
-            
 
 def main():
 
@@ -60,7 +102,3 @@ def main():
 
     for key in diff_dct:
         print("{0}: {1}".format(key, diff_dct[key]))
-
-
-if __name__ == "__main__":
-    main()
